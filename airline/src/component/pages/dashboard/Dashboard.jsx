c4efd7c283f73979c1369d33b773010733cee842
TasnimZaid <tasnim.aboarqob@gmail.com>
2024-07-18 19:56:06 +0300

    with router and Folders

---
+++ airline/src/component/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'; 
+import React from 'react';
+
+function Dashboard(){
+    return(
+        <>
+         Hello
+        </>
+    )
+
+}
+export default Dashboard ;
---
+++ airline/src/component/pages/homePage/Homepage.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'; 
+import React from 'react';
+
+function Home(){
+    return(
+        <>
+        hello
+         <ul>
+            
+           <li className="mr-5"><Link to="/Home" className="no-underline text-black">Home</Link></li>
+         </ul>
+        </>
+    )
+
+}
+export default Home ;
